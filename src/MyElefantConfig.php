ed5701f1ed8ff3939a00dbe602072195a6a2bc4d
PREFOL <timothy.prefol@gmail.com>
2019-03-20 14:02:50 +0100

    Fixed bug phone number

--- src/MyElefantConfig.php
+++ src/MyElefantConfig.php
@@ -5,19 +5,17 @@ namespace myelefant;
 abstract class MyElefantConfig
 {
     const CREDIT_REMAINING = 'remaining credit';
-    const CRITICAL_MESSAGE_CONTACT_FORMAT = 'Contact must be a multidimensional array. 
-                                             Example [[\'33601020304\', \'John\',\'Doe\']]';
-    const CRITICAL_MESSAGE_CONTENT_FORMAT = 'Content must be an array and first parameter must be a phone number
-                                             Example [\'33601020304\',\'First content\',\'...\']';
+    const CRITICAL_MESSAGE_CONTACT_FORMAT = 'Contact must be a multidimensional array. Example [[\'33601020304\', \'John\',\'Doe\']]';
+    const CRITICAL_MESSAGE_CONTENT_FORMAT = 'Content must be an array and first parameter must be a mobile phone number. Example [\'33601020304\',\'First content\',\'...\']';
     const CRITICAL_MESSAGE_DATE = 'Date is before the current date';
     const CRITICAL_MESSAGE_DATE_FORMAT = 'Format Date invalid expected yyyy-mm-dd HH:MM';
-    const CRITICAL_MESSAGE_PHONE_NUMBER_FORMAT = 'Format phone number invalid expected 33612232305';
+    const CRITICAL_MESSAGE_PHONE_NUMBER_FORMAT = 'Format invalid, expected MOBILE phone number. Example \'33612232305\'';
     const CRITICAL_MESSAGE_EMPTY_MESSAGE = 'If you specify a sender the message cannot be empty';
     const CRITICAL_MESSAGE_EMPTY_SECRET_KEY = 'You must provide secret key';
     const MAX_LENGTH_MESSAGE = 160;
     const PATH_ERROR_LOG = 'var/log/error.log';
     const PATH_INFO_LOG = 'var/log/info.log';
-    const REGEX_PHONE_NUMBER = '/^[0-9]{2}[0-9]{9}$/';
+    const REGEX_PHONE_NUMBER = '/^(33)[67](\d{2}){4}$/';
     const SUCCESS_MESSAGE = 'Message sent correctly';
     const URL_MYELEFANT_API = 'https://api.myelefant.com';
     const URL_MYELEFANT_API_AUTHENTIFICATION = '/v1/token';
